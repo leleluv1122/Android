c447d4d81f250d8e80128aa53879341e75dbdc0b
leleluv1122 <jh_o214@naver.com>
2020-01-02 15:33:04 +0900

    leleluv1122

--- View/app/src/main/java/com/example/view/MainActivity.java
+++ View/app/src/main/java/com/example/view/MainActivity.java
@@ -41,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, CheckboxesActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_spinners) {
+            Intent intent = new Intent(this, SpinnersActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_alerts) {
+            Intent intent = new Intent(this, AlertsActivity.class);
+            startActivity(intent);
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
